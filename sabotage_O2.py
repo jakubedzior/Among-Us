2addfa6aaeb27e401bcc58931be1bb21fa7acdd6
Jakub Kędzior <jakubedzior@gmail.com>
2021-02-17 04:28:35 +0100

    Update sabotage_O2.py
    optimized for adding more screens; added filters

--- sabotage_O2.py
+++ sabotage_O2.py
@@ -1,19 +1,36 @@
 import pyautogui as py
 import ctypes
-from time import sleep
 import pytesseract
+from PIL import ImageFilter
 
 
 def fixO2_method():
+    def getScreenCredentials(upper_left: tuple, bottom_right: tuple): 
+        '''
+        upper_left: position of upper-left button (1)
+        bottom_right: lenght, position of bottom_right button (confirm)
+        '''
+        space = ((bottom_right[0] - upper_left[0]) / 2, (bottom_right[1] - upper_left[1]) / 3)
+
+        buttons = []
+        for i in range(4):
+            for j in range(3):
+                buttons.append((upper_left[0] + space[0] * j, upper_left[1] + space[1] * i))
+        buttons.insert(0, buttons[-2])
+        del buttons[-3:-1]
+        
+        return tuple(buttons)
+
+
     user32 = ctypes.windll.user32
     user32.SetProcessDPIAware()
     screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-    if screensize == (1980, 1080):
-        region = ()
-        buttons = ()
+    if screensize == (1920, 1080):
+        region = (1300, 485, 1425, 525)
+        buttons = getScreenCredentials((800, 380), (1125, 870))
     elif screensize == (2736, 1824):
         region = (1900, 830, 2120, 900)
-        buttons = ((1370, 1440), (1100, 660), (1370, 660), (1620, 670), (1100, 910), (1370, 910), (1620, 910), (1100, 1180), (1370, 1180), (1620, 1180), (1620, 1440))
+        buttons = getScreenCredentials((1100, 660), (1620, 1440))
     else:
         raise IndexError("Your screen's size is not supported.")
 
@@ -22,13 +39,17 @@ def fixO2_method():
     img = img.rotate(-25)
     img = img.crop(region)
     img = img.convert('LA')
-    img = img.point(lambda p: p > 90 and 255)
+    img = img.point(lambda p: p > 100 and 255)
+    img = img.filter(ImageFilter.GaussianBlur(radius=1))
+    img = img.point(lambda p: p > 200 and 255)
     # img = img.resize((img.size[0]*2, img.size[1]*2), Image.ANTIALIAS)
+    # img.show()
+
 
     # pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
     string = pytesseract.pytesseract.image_to_string(img, config='-c tessedit_char_whitelist=0123456789')
     string = string[:5]
-
+    
     if string.isnumeric():
         number = [int(sign) for sign in string]
     else:
@@ -37,7 +58,6 @@ def fixO2_method():
 
     for digit in number:
         py.click(buttons[digit])
-        sleep(0.05)
     py.click(buttons[-1])
 
     img.save(f'Screenshots/o2/{string}.png')
